c0d2f9abdb430601cd804c2e0886d60ffd8a57b4
Daniil <yourdallero@gmail.com>
2021-11-14 13:28:26 +0700

    HomeWork09

--- app/src/main/java/ru/klimov/exitapplication/MainActivity.java
+++ app/src/main/java/ru/klimov/exitapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package ru.klimov.exitapplication;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setMessage("Вы действительно хотите выйти?")
+                .setCancelable(false)
+                .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "Выходим...", Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                }).setNegativeButton("Нeт", null).show();
+    };
 }
